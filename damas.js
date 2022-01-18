e050525cc41ba0dc9abc58380d4919a67cf089b1
Camila Egydio <91480234+cfegydio@users.noreply.github.com>
2022-01-18 11:14:42 -0300

    Update damas.js

--- damas.js
+++ damas.js
@@ -1,7 +1,41 @@
 const tamanhoCelula = 40;
-document.body.append(tabuleiro());
+document.body.append(criarTabuleiro());
 
-function criarPecas(cor) {
+function criarTabuleiro() {
+    const tamanho = 8;
+    let tabela = document.createElement('table');
+
+	tabela.style.borderStyle = 'solid';
+	tabela.style.borderSpacing = 0;
+	tabela.style.margin = 'auto';
+	tabela.style.borderColor = 'black';
+
+for (let i = 0; i < tamanho; i++) {
+        let linha = document.createElement('tr');
+        tabela.append(linha);
+        for (let j = 0; j < tamanho; j++) {
+            let celula = document.createElement('td');
+            linha.append(celula);
+            celula.style.width = `${tamanhoCelula}px`;
+            celula.style.height = `${tamanhoCelula}px`;
+            if (i % 2 == j % 2) {
+                celula.style.backgroundColor = '#FA8072';
+                celula.classList.add("container");
+                if (i * 8 + j <= 24) {
+                    celula.append(criaPeca('black'));
+                } else if (i * 8 + j >= 40) {
+                    celula.append(criaPeca('red'));
+                }
+            } else {
+                celula.style.backgroundColor = 'white';
+            }
+        }
+    };
+    return tabela;
+}
+
+
+function criaPeca(cor) {
     let imagem = document.createElement('img');
     imagem.setAttribute('src', `img/${cor}.png`);
     imagem.setAttribute('width', `${tamanhoCelula-4}px`);
@@ -33,36 +67,3 @@ containers.forEach(container => {
         container.appendChild(draggable);
     });
 });
-
-function tabuleiro() {
-    const tamanho = 8;
-    let tabela = document.createElement('table');
-
-    tabela.style.borderStyle = 'solid';
-	tabela.style.borderSpacing = 0;
-	tabela.style.margin = 'auto';
-	tabela.style.borderColor = 'black';
-
-for (let i = 0; i < tamanho; i++) {
-        let linha = document.createElement('tr');
-        tabela.append(linha);
-        for (let j = 0; j < tamanho; j++) {
-            let celula = document.createElement('td');
-            linha.append(celula);
-            celula.style.width = `${tamanhoCelula}px`;
-            celula.style.height = `${tamanhoCelula}px`;
-            if (i % 2 == j % 2) {
-                celula.style.backgroundColor = '#FA8072';
-                celula.classList.add("container");
-                if (i * 8 + j <= 24) {
-                    celula.append(criaPeca('black'));
-                } else if (i * 8 + j >= 40) {
-                    celula.append(criaPeca('red'));
-                }
-            } else {
-                celula.style.backgroundColor = 'white';
-            }
-        }
-    };
-    return tabela;
-}
